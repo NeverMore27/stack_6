77da5e9d8f3956c87fdfbd682b8c8f4c24be528a
NeverMore27 <uragan.uragan.59@mail.ru>
2017-11-04 00:46:57 +0300

    Update stack.hpp

--- include/stack.hpp
+++ include/stack.hpp
@@ -1,29 +1,126 @@
-#ifndef STACK_HPP 
-#define STACK_HPP
+#include <iostream>
+#include <string>
+#include <mutex>
+#include <memory>
 
 template <typename T>
 class stack
 {
 public:
-    using size_type = std::size_t;
-    
-    stack();
-    bool empty() const;
+	stack() /*noexcept*/ noexcept; 
+	~stack()  /*noexcept*/ noexcept;
+	stack(const stack<T>&) /*strong*/;
+	stack<T>& operator =(const stack<T>&) noexcept;
+	void push(T const &) /*strong*/;
+	auto pop() -> std::shared_ptr<T>;
+	size_t size() const /*noexcept*/ noexcept;
+	bool empty() const /*noexcept*/ noexcept;
 private:
-    T * buffer_;
-    size_type buffer_size_;
-    size_type count_;
+	mutable std::mutex mutex_;
+	T * array_;
+	size_t array_size_;
+	size_t count_;
+	void swap(stack<T>&) /*noexcept*/ noexcept;
 };
 
 template <typename T>
-stack<T>::stack() : buffer_{ nullptr }, buffer_size_ { 0UL }, count_{ 0UL }
+void stack<T>::swap(stack<T>& object) noexcept
+{		
+	std::lock(mutex_, object.mutex_); 
+	std::swap(object.array_size_, array_size_);
+	std::swap(count_, object.count_);
+	std::swap(object.array_, array_);
+	mutex_.unlock();
+	object.mutex_.unlock();
+}
+
+template <typename T>
+stack<T>::stack() noexcept: count_{0}, array_size_{0}, array_{nullptr} 
 {
 }
 
 template <typename T>
-bool stack<T>::empty() const
+stack<T>::~stack() noexcept
 {
-    return count_ == 0;    
+	delete[] array_;
 }
 
-#endif
+template <typename T>
+stack<T>::stack(const stack& object)
+{
+	std::lock_guard<std::mutex> lock(object.mutex_);
+	array_size_ = object.array_size_;
+	count_ = object.count_;
+	array_= new T[count_];
+	try 
+	{
+		std::copy(object.array_,object.array_+count_, array_);
+	}
+	catch (...)
+	{ 
+      		delete[] array_;
+		throw;
+	}
+}
+
+template <typename T>
+stack<T>& stack<T>:: operator =(const stack<T>&object) noexcept
+{	
+	if (this != &object)
+	{
+		stack{object}.swap(*this);
+	}
+	return *this;
+}
+
+template <typename T>
+void stack<T>::push(T const &value)
+{
+	std::lock_guard<std::mutex> lock(mutex_);
+	if (array_size_ == count_)
+	{
+		size_t array_size = array_size_ == 0 ? 1 : array_size_ * 2;
+		T *ptr = new T [array_size_];
+		try 
+		{
+			std::copy(array_, array_ + count_, ptr);
+		}
+		catch (...)
+		{  
+      			delete[] ptr;
+			throw;
+		}
+		
+		array_size_=array_size;
+		delete[] array_;
+		array_ = ptr;
+	}
+	
+	array_[count_] = value;
+	++count_;
+}
+
+template <typename T>
+auto stack<T>::pop() -> std::shared_ptr<T> 
+{
+	std::lock_guard<std::mutex> lock(mutex_);
+	if (count_==0) throw ("Stack is empty");
+	auto ar = std::make_shared<int>(array_[count_-1]);
+	--count_;
+	return ar;
+}
+
+template <typename T>
+size_t stack<T>::size() const noexcept
+{
+	std::lock_guard<std::mutex> lock(mutex_);
+
+	return count_;
+}
+
+template <typename T>
+bool stack<T>::empty() const noexcept
+{	
+	std::lock_guard<std::mutex> lock(mutex_);
+	return count_==0;
+}
