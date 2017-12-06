81d66efdf8e3ae6dd98164b1ab163977654d5272
NeverMore27 <uragan.uragan.59@mail.ru>
2017-12-06 23:44:30 +0300

    Update init.cpp

--- tests/init.cpp
+++ tests/init.cpp
@@ -1,37 +1,98 @@
 
 #include <stack.hpp>
 #include <catch.hpp>
-SCENARIO("ini")
+
+SCENARIO("init") 
 {
-	stack<int> Stack;
-	REQUIRE(Stack.size() == 0);
-	REQUIRE(Stack.empty());
-}
-	
-SCENARIO("push")
-{
-	stack<int> s;
-	s.push(4);
-	s.push(5);
-	s.push(12);
-	s.push(78);
-	auto sp =s.pop();
-	REQUIRE(s.size() == 3);
-	REQUIRE(*sp==78);
-}
-SCENARIO("count and size")
-{
-	stack<int> Stack;
-	Stack.push(5);
-	Stack.push(7);
-	REQUIRE(Stack.size() == 2);
+    forward_list<int> s;
+    REQUIRE(s.count() == 0);
+    REQUIRE(s.empty());
 }
 
-SCENARIO("pop")
+SCENARIO("pop") 
 {
-	stack<int> Stack;
-	Stack.push(5);
-	Stack.push(7);
-	REQUIRE(Stack.size() == 2);
+    forward_list<int> v={1, 2, 4};
+    int s = v.pop_back();
+    int z = v.pop_front();                     
+    REQUIRE(s== 4);
+    REQUIRE(v.count() == 1);                
+    REQUIRE(z==1);
+}
+SCENARIO("init list") 
+{
+    forward_list<int> v={1, 2, 3, 4, 5, 6};
+    int s = v.pop_back();
+    REQUIRE(v.count() == 5);
+    REQUIRE(s == 6);
+    s = v.pop_back();
+    REQUIRE(s == 5);
+    REQUIRE(v.count() == 4);
+    s = v.pop_back();
+    REQUIRE(s == 4);
+    REQUIRE(v.count() == 3);
+    s = v.pop_back();
+    REQUIRE(s == 3);
+    REQUIRE(v.count() == 2);
+    s = v.pop_back();
+    REQUIRE(s == 2);
+    REQUIRE(v.count() == 1);
+    s = v.pop_back();
+    REQUIRE(s == 1);
+    REQUIRE(!v.empty());
 }
 
+SCENARIO("push") 
+{
+   forward_list<int> s;
+    s.push_back(5);
+    s.push_front(7);
+    REQUIRE(s.count() == 2);
+}
+SCENARIO("clear") 
+{
+   forward_list<int> s;
+   s.push_back(5);
+   s.push_front(7);
+   s.clear();
+   REQUIRE(s.count() == 0);
+}
+ SCENARIO("move") 
+{
+   forward_list<int> s = {1, 2};
+   forward_list<int> m =std:: move(s);
+   REQUIRE(m.count()==2);
+}
+SCENARIO("copy") 
+{
+   forward_list<int> s = {1, 2, 5, 9};
+   forward_list<int> m =s;
+   REQUIRE((m == s));
+}
+SCENARIO("back") 
+{
+   forward_list<int> s = {1, 2, 4};
+   forward_list<int> m;
+   forward_list<int> m2;
+   m.push_back(1);
+   m.push_back(2);
+   m.push_back(4);
+   m.emplace_back(1);
+   m.emplace_back(2);
+   m.emplace_back(4);
+   REQUIRE((m == s));
+   REQUIRE((m2 == s));
+}
+SCENARIO("front") 
+{
+   forward_list<int> s = {4, 2, 1};
+   forward_list<int> m;
+   forward_list<int> m2;
+   m.push_front(1);
+   m.push_front(2);
+   m.push_front(4);
+   m.emplace_front(1);
+   m.emplace_front(2);
+   m.emplace_front(4);
+   REQUIRE((m == s));
+   REQUIRE((m2 == s));
+}
