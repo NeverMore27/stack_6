6a142b57c99d963b6a3c65f51168aefcf30676c4
NeverMore27 <uragan.uragan.59@mail.ru>
2017-12-06 23:43:06 +0300

    Update example.cpp

--- examples/example.cpp
+++ examples/example.cpp
@@ -4,39 +4,3 @@
 
 
 
-void producer (stack<int> &st)
-{
-   for (;;)
-    {
-     	    st.push(rand()%10);
-      	    std::this_thread::sleep_for(std::chrono::seconds(std::rand() % (3) + 1));
-    }
-}
-
-void consumer (stack<int> &st)
-{
-   for (;;)
-    {
-	   try
-	   { 
-       		st.pop();
-	   }
-	   catch(...)
-	   {
-		 std::this_thread::sleep_for(std::chrono::seconds(std::rand() % (3) +2 ));   
-	   }
-      	   std::this_thread::sleep_for(std::chrono::seconds(std::rand() % (3) +2 ));
-    }
-}
-
-int main()
-{
-	stack<int> Stack;
-	
-	std::thread p(producer, std::ref(Stack));
-	std::thread c(consumer, std::ref(Stack));
-	
-	p.join();
-	c.join();
-  return 0;
-}
